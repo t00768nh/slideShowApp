aca3b2ec4d2fec25329cd0cc3f7be47db4622108
hattori <t00768nh@gmail.com>
2016-10-26 00:32:33 +0900

    Initial Commit

---
+++ SlidshowApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlidshowApp
+//
+//  Created by t on 2016/10/26.
+//  Copyright © 2016年 hattori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
